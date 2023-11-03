4ed0c0f6571f2f694b7630ee4eaa90ffcedd27a9
min <myun02302@gmail.com>
2023-11-03 20:40:57 +0900

    Fix : cors origin과 credentials 규칙 추가
    로그아웃 기능을 구현할 때 back단에서 쿠키에 접근할 수 없었다. 이유는 cors의 credentials 설정을 안해줘서 발생했다.

--- backend/index.js
+++ backend/index.js
@@ -6,16 +6,18 @@ import connectDB from './mongodb/connect.js'
 import postRoutes from './routes/postRoutes.js'
 import dalleRoutes from './routes/dalleRoutes.js'
 import oauthRoutes from './routes/oauthRoutes.js'
+import usersRoutes from './routes/usersRoutes.js'
 
 dotenv.config()
 
 const app = express();
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 app.use(express.json({ limit: '50mb' }))
 
 app.use('/api/v1/posts', postRoutes)
 app.use('/api/v1/dalle', dalleRoutes)
 app.use('/api/v1/oauth', oauthRoutes)
+app.use('/api/v1/users', usersRoutes)
 
 app.get('/', async (req, res) => {
   res.status(200).json({
