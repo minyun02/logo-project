048aec22a5fea5d2c73196c3a0802b9793af1aff
min <myun02302@gmail.com>
2023-11-05 19:46:21 +0900

    Refactor: Cloudinary 설정 로직 분할
    라우터에서 Cloudinary 설정하던 부분을 별도의 js 파일로 분리

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
-import { v2 as cloudinary } from 'cloudinary'
+import { configureCloudinary } from '../config/cloudinary.js'
 
 import Post from '../models/Post.js'
 import User from '../models/User.js'
@@ -8,12 +8,7 @@ import User from '../models/User.js'
 dotenv.config()
 
 const router = express.Router()
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
+const cloudinary = configureCloudinary()
 
 router.route('/').get(async(req, res) => {
   try {
@@ -29,7 +24,6 @@ router.route('/').post( async (req, res) => {
   try {
     const userId = req.headers.userid
     const user = await User.findOne({ userId: userId })
-  
     if (!user) {
       res.status(401).json({ success: false, message: "존재하지 않는 회원입니다."})
     }
@@ -55,7 +49,6 @@ router.route('/').post( async (req, res) => {
 router.route('/my').get( async (req, res) => {
   try {
     const userId = req.headers.userid
-  
     const myPosts = await Post.find({userId: userId})
     
     res.status(200).json({ success: true, data: myPosts })
--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
-import { v2 as cloudinary } from 'cloudinary'
 
 import { COOKIE_NAME, USER_COOKIE } from '../utils/constants.js'
 import Post from '../models/Post.js'
@@ -8,12 +7,6 @@ import Post from '../models/Post.js'
 dotenv.config()
 
 const router = express.Router()
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
 
 router.route('/').get(async (req, res) => {
   try {
@@ -35,7 +28,7 @@ router.route('/logout').get(async (req, res) => {
     res.clearCookie(USER_COOKIE, {
       path: '/', domain: 'localhost'})
 
-      return res.status(200).json({ success: true })
+    return res.status(200).json({ success: true })
   } catch (error) {
     console.log(error)
   }
